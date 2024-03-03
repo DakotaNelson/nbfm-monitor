afc408dc2981eaa29cbc367ddafd5925c6a9b285
Dakota Nelson <dakota.w.nelson@gmail.com>
2024-03-03 07:48:33 -0800

    add initial FFT setup

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ use byteorder::{LittleEndian, WriteBytesExt};
 use num_complex::Complex;
 use soapysdr::Direction::Rx;
 use rustfft::FftPlanner;
+use simple_moving_average::{SMA, NoSumSMA};
 
 fn main() {
     println!("Starting nbfm-listener...");
 
-    let dev_filter = "";
+    let dev_filter = "driver=rtlsdr";
     let channel = 0;
     let fname = "testfile";
     let mut num_samples = i64::MAX;
@@ -48,38 +49,45 @@ fn main() {
     let mut stream = dev.rx_stream::<Complex<f32>>(&[channel]).unwrap();
     let mut buf = vec![Complex::new(0.0, 0.0); stream.mtu().unwrap()];
 
-    let mut outfile = BufWriter::new(File::create(fname).expect("error opening output file"));
+    //let mut outfile = BufWriter::new(File::create(fname).expect("error opening output file"));
 
     stream.activate(None).expect("failed to activate stream");
 
     while num_samples > 0 && !sb.caught() {
         let read_size = min(num_samples as usize, buf.len());
         let len = stream.read(&mut [&mut buf[..read_size]], 1_000_000).expect("error reading stream");
-        write_complex_file(&buf[..len], &mut outfile).unwrap();
+        //write_complex_file(&buf[..len], &mut outfile).unwrap();
         num_samples -= len as i64
     }
 
-    stream.deactivate(None).expect("failed to deactivate stream");
+    // TODO move fft into the loop above for continuous operation
 
-    // todo - FFT
+    stream.deactivate(None).expect("failed to deactivate stream");
+    println!("stream deactivated, starting FFT...");
+
     // fft[0] -> DC
     // fft[len/2 + 1] -> nyquist f
     // 2 - len/2 are positive frequencies, each step is samp_rate/len(fft)
     // https://www.gaussianwaves.com/2015/11/interpreting-fft-results-complex-dft-frequency-bins-and-fftshift/
-    const FFT_SIZE: usize = SAMP_RATE;
+    let fft_size = buf.len();
     let mut planner = FftPlanner::new();
-    let fft = planner.plan_fft_forward(FFT_SIZE);
+    let fft = planner.plan_fft_forward(fft_size);
 
     // TODO optimize using process_with_scratch()
     fft.process(&mut buf);
 
-    let fftbuf: [f32; FFT_SIZE];
-    fftbuf = buf.iter().map(|val| {val.norm()}).collect::<Vec<f32>>().try_into().unwrap();
+    let fftbuf: Vec<f32> = buf.iter().map(|val| {val.norm()}).collect();
 
-    let mut fftoutfile = BufWriter::new(File::create(fname).expect("error opening output file"));
+    let mut fftoutfile = BufWriter::new(File::create("fft-output").expect("error opening output file"));
     write_scalar_file(&fftbuf, &mut fftoutfile).expect("failed to write fft output");
 
-    // TODO running average of FFT values
+    // keep a running average of FFT values
+    const WINDOW_SIZE: usize = 10;
+    let mut fft_averages = vec!(NoSumSMA::<f32, f32, WINDOW_SIZE>::new(); fft_size);
+
+    for (index, element) in fftbuf.into_iter().enumerate() {
+        fft_averages[index].add_sample(element);
+    }
 }
 
 fn write_scalar_file<W: Write>(src_buf: &[f32], mut dest_file: W) -> io::Result<()> {
